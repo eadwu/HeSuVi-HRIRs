f21db8cb53dccc68bc4aff35bef2237a3d2f2b23
Edmund Wu <fangkazuto@gmail.com>
2020-11-11 15:15:07 -0500

    scripts: include conversion script

---
+++ scripts/convert.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env nix-shell
+#! nix-shell ../shell.nix -i bash
+
+for file in hrir/*.wav; do
+  basename="$(basename "${file}" .wav)"
+
+  mkdir -p "7.0-stereo-pairs/${basename}"
+  hesuvi_convert "$file" "7.0-stereo-pairs/${basename}/${basename}_L.wav" "7.0-stereo-pairs/${basename}/${basename}_R.wav"
+done
