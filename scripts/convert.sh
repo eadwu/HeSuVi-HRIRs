1c05524866d0ca854eee7c0aafcc6850a045be33
Edmund Wu <fangkazuto@gmail.com>
2020-11-13 14:24:30 -0500

    scripts/convert: verbosify progress

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -4,6 +4,7 @@
 for file in hrir/*.wav; do
   basename="$(basename "${file}" .wav)"
 
+  printf "Converting %s\n" "$file"
   mkdir -p "7.0-stereo-pairs/${basename}"
   hesuvi_convert "$file" "7.0-stereo-pairs/${basename}/${basename}_L.wav" "7.0-stereo-pairs/${basename}/${basename}_R.wav"
 done
